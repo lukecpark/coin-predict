aac767339f5ada6134bef9fc8d262d357e85f891
박상현 <twodude@naver.com>
2018-02-15 04:10:30 +0900

    Update up_down.py

--- coin/up_down.py
+++ coin/up_down.py
@@ -1,28 +1,21 @@
 import os
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from keras.callbacks import EarlyStopping
 from keras.models import Sequential
-from keras.layers import Dropout
 from keras.layers.core import Dense
-from keras.layers.recurrent import LSTM
-from keras.optimizers import adam
+from keras.layers.recurrent import GRU
+from keras.utils import np_utils
 import matplotlib.pyplot as plt
-from math import sqrt
 
-SEED = 7 # random seed
-np.random.seed(SEED)
 
-# main
-if __name__ == "__main__":
+def load_data(window):
     """
     데이터 읽기
     """
     PATH = './result/'
     # open, high, low, close, volume
-    raw = np.loadtxt(PATH + 'output.csv', delimiter=',', skiprows=1, usecols=[1, 2, 3, 4, 5])
+    raw = np.loadtxt(PATH + 'output.csv', delimiter=',', skiprows=1, usecols=[4, 5])  # select close, volume
 
     """
     데이터 정규화
@@ -33,132 +26,159 @@ if __name__ == "__main__":
     """
     데이터 생성
     """
-    seq_len = len(raw) # 전체 시계열 길이
-    window = 7 # 하나의 시계열 길이
-    dim = 5 # 데이터 차원
+    seq_len = len(raw)  # 전체 시계열 길이
+    dim = 2  # 데이터 차원(close, volume)
 
     X = []
-    Y = []
+    Y = []  # up: 0, down: 1
+    y_prev = nptf[6][0]  # initialization
+
     for i in range(seq_len - window):
         x_ = nptf[i: i + window]
-        y_ = nptf[i + window]
         X.append(x_)
-        Y.append(y_)
-        # print(x_, '->', y_)
+
+        y_ = nptf[i + window][0]
+        if y_ > y_prev:
+            Y.append(0)
+        else:
+            Y.append(1)
+
+        y_prev = y_
+
+    encoder = LabelEncoder()
+    encoder.fit(Y)
+    encoded_Y = encoder.transform(Y)
+    dummy_Y = np_utils.to_categorical(encoded_Y)  # one hot encoded
 
     X = np.array(X)
-    Y = np.array(Y)
+    Y = dummy_Y
 
-    # split to train and testing
-    N_test = int(len(Y) * 0.2)
-    N_val = int((len(Y) - N_test) * 0.1)
-    # X_train, X_val, X_test = np.array(X[:N_train]), np.array(X[N_train:N_train+N_val]), np.array(X[N_train+N_val:])
-    # Y_train, Y_val, Y_test = np.array(Y[:N_train]), np.array(Y[N_train:N_train+N_val]), np.array(Y[N_train+N_val:])
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=N_test, random_state=SEED)
-    X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=N_val, random_state=SEED)
+    return X, Y
 
-    """
-    모델 설정
-    """
-    n_in = len(X[0][0])
-    n_out = len(Y[0])
-    n_hidden = [20, 20, 20, 20, 20]
 
-    early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
-
+def modeling(window, n_in, n_hidden, n_out):
     """
     모델 구축
     """
     model = Sequential()
 
-    model.add(LSTM(
+    model.add(GRU(
         n_hidden[0],
         input_shape=(window, n_in),
         activation='softsign',
+        # dropout=0.5,
         return_sequences=True))
-    model.add(Dropout(0.3))
+    # model.add(Dropout(0.5))
 
     for i in range(len(n_hidden) - 2):
-        model.add(LSTM(
+        model.add(GRU(
             n_hidden[i + 1],
             activation='softsign',
+            # dropout=0.5,
             return_sequences=True))
-        model.add(Dropout(0.3))
+        # model.add(Dropout(0.5))
 
-    model.add(LSTM(
+    model.add(GRU(
         n_hidden[len(n_hidden) - 1],
         activation='softsign',
         return_sequences=False))
 
     model.add(Dense(
         n_out,
-        activation='linear'))
+        activation='softmax'))
 
-    model.summary()
+    return model
 
-    optimizer = adam(lr=0.005)
-    model.compile(loss='mean_squared_error', optimizer=optimizer)
 
+def load_weight(model):
     """
-    모델 학습
+    가중치 load
     """
-    history = model.fit(X_train, Y_train,
-                        validation_data=(X_val, Y_val),
-                        epochs=200,
-                        callbacks=[early_stopping],
-                        verbose=2)
+    IN_PATH = './model/'
+    try:
+        model.load_weights(IN_PATH + 'gru_weights.h5')
+        print('Load: gru_weights.h5')
+    except:
+        print('New model')
 
-    """
-    Loss
-    """
-    plt.plot(history.history['loss'])
-    plt.title('Loss')
-    plt.show()
+    return model
 
-    """
-    모델 평가
-    """
-    output_ = scaler.inverse_transform(model.predict(X_test))
-    target_ = scaler.inverse_transform(Y_test)
-    score = sqrt(mean_squared_error(target_[::, 3], output_[::, 3]))
-    print('Train Score: %.2f RMSE' % score)
 
+def save_weight(model):
     """
-    모델 저장
+    가중치 save
     """
     OUT_PATH = './model/'
     if not os.path.exists(OUT_PATH):
         os.makedirs(OUT_PATH)
 
-    model.save(OUT_PATH + 'lstm.h5')
-    print('Create: lstm.h5')
+    model.save_weights(OUT_PATH + 'gru_weights.h5')
+    print('Save: gru_weights.h5')
 
+
+def plot_hist(hist):
     """
-    학습 후 출력
+    Accuracy & Loss
     """
-    output = scaler.inverse_transform(model.predict(X))
-    target = scaler.inverse_transform(Y)
-    plt.plot(output[::, 3], 'b--', label='output')
-    plt.plot(target[::, 3], 'r-', label='target')
+    fig, loss_ax = plt.subplots()
+    acc_ax = loss_ax.twinx()
 
-    """
-    향후 예측
-    """
-    future = 30
-    predict = [None for i in range(len(Y) - 1)]
+    loss_ax.plot(hist.history['loss'], 'y', label='loss')
+    acc_ax.plot(hist.history['acc'], 'b', label='acc')
 
-    z_ = X[-1:]  # 가장 뒤
-    for i in range(future):
-        y_ = model.predict(z_)
+    loss_ax.set_xlabel('epoch')
+    loss_ax.set_ylabel('loss')
+    acc_ax.set_ylabel('accuracy')
 
-        z_ = np.append(z_[0], y_, axis=0)
-        z_ = z_[np.newaxis, 1:]
-
-        y_ = scaler.inverse_transform(y_)
-        predict.append((y_.reshape(-1))[3])
-
-    # graph
-    plt.plot(predict, 'g--', label='predict')
-    plt.legend()
-    plt.title('After Training - close')
+    loss_ax.legend(loc='upper left')
+    acc_ax.legend(loc='lower left')
     plt.show()
+
+
+# main
+if __name__ == "__main__":
+    window = 7  # 하나의 시계열 길이
+
+    """
+    데이터 로드
+    """
+    X, Y = load_data(window)
+
+    N_test = int(len(Y) * 0.2)  # split to train and testing
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=N_test)
+
+    """
+    모델 구축
+    """
+    n_in = len(X[0][0])
+    n_hidden = [20, 20, 20, 20, 20]
+    n_out = len(Y[0])
+
+    model = modeling(window, n_in, n_hidden, n_out)  # 모델 구축
+    model.summary()
+
+    # regression: linear, mean_squared_error
+    # classification: softmax, binary_crossentropy
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    """
+    모델 학습
+    """
+    epochs = 100
+    units = 100  # save weights per each epochs/units
+
+    for i in range(int(epochs / units)):
+        model = load_weight(model)
+        hist = model.fit(X_train, Y_train, epochs=units, verbose=2)
+        save_weight(model)
+
+    plot_hist(hist)  # draw plot
+
+    """
+    모델 평가
+    """
+    scores = model.evaluate(X_test, Y_test, verbose=2)
+    print('\n%s: %.2f%%' % (model.metrics_names[1], scores[1] * 100))
+
+    predict = model.predict(X[-1:])  # 예측
+    print('UP: %.2f%%, DOWN: %.2f%%' % (predict[0][0], predict[0][1]))  # 확률: [up, down]
